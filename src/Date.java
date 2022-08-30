92e21886839fdd5896d533174d783149f2696edf
Nathally Nunes <nathally.nunes@gmail.com>
2022-08-30 12:18:44 -0300

    Simple Exercise

---
+++ src/Date.java
@@ -0,0 +1,13 @@
+public class Date {
+
+    public static void main(String[] args) {
+
+        String day = "Friday", month = "November";
+        int date = 20, year = 2021;
+
+        System.out.println("American format: ");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println("European format: ");
+        System.out.println(day + " " + date + " " + month + " " + year);
+    }
+}
